15b649eef3c747762f1efdd964b7606f9e351534
Cristiana Costa <cristianacmc@gmail.com>
2019-03-20 18:10:44 +0000

    Add nested for loop X exercise

---
+++ Week3/NestedForX.java
@@ -0,0 +1,22 @@
+package Week3;
+
+/**
+ * Created by Cristiana Costa
+ * on 2019-03-20
+ * Program to print out a table of 'x' in 3 rows and 4 columns
+ */
+
+public class NestedForX {
+
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 3; i++){
+            for(int j = 0; j < 4; j ++){
+                System.out.print("\tX");
+            }//for i
+            System.out.println();
+        }//forj
+        
+    } //main
+
+} //class
