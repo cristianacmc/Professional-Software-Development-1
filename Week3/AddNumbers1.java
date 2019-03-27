f15c26d2116b4a98563a7b014cd28f5cd6ba2d3d
Cristiana Costa <cristianacmc@gmail.com>
2019-03-27 12:12:30 +0000

    Add repetition exercises

--- Week3/AddNumbers1.java
+++ Week3/AddNumbers1.java
@@ -1,14 +1,22 @@
 package Week3;
 
+import java.util.Scanner;
+
 /**
  * Created by Cristiana Costa on 2019-03-05
- * COMMENTS ABOUT PROGRAM HERE
- * Program to
+ * Program to read in 3 numbers and print out their total
  */
 
 public class AddNumbers1 {
 
     public static void main(String[] args) {
+        int total = 0;
+        Scanner scan = new Scanner(System.in);
+        for(int i = 0; i < 3; i++){
+            System.out.print("Enter a number: ");
+            total += scan.nextInt();
+        }
+        System.out.println("The total of the numbers is: " + total);
     } //main
 
 } //class
--- Week3/OddNumbers.java
+++ Week3/OddNumbers.java
@@ -2,13 +2,23 @@ package Week3;
 
 /**
  * Created by Cristiana Costa on 2019-03-05
- * COMMENTS ABOUT PROGRAM HERE
- * Program to
+ * Program to print out all of the odd numbers between 1 and 50
  */
 
 public class OddNumbers {
 
     public static void main(String[] args) {
+        int total = 0;
+        int cont = 0;
+        for (int n = 1; n <= 50; n ++){
+            if(n % 2 !=0){
+                System.out.println(n);
+                total += n;
+                cont ++;
+            }
+        }
+        System.out.println("The total of these numbers is: " +total);
+        System.out.println("The average of theese numbers is " + total/cont);
 
     } //main
 
